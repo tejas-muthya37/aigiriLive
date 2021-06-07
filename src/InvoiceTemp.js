d75aca7c479bf447bc2299c13a79832ed5965ff9
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2021-06-07 10:49:12 +0530

    mp on heroku2

--- src/InvoiceTemp.js
+++ src/InvoiceTemp.js
@@ -186,7 +186,7 @@ function InvoiceTemp() {
                 <input type="hidden" name="GRAND-TOTAL" value={userTotal + shippingCost} />
                 <input type="hidden" name="_subject" value="Your order from Madhwa Paaka" />
                 <input type="hidden" name="_autoresponse" value="Thank you for shopping with Madhwa Paaka. This is a confirmation mail that we have successfully received your order." />
-                <input type="hidden" name="_next" value="http://www.madhwapaaka.in"/>
+                <input type="hidden" name="_next" value="https://madhwapaaka.herokuapp.com/success"/>
             </form>
 
             <div className="invoice_temp_top">
