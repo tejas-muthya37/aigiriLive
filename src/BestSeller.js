b2bea281040a6d3e7aeb6f1b711e3547615a264a
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2021-06-13 09:03:32 +0530

    minor-changes

--- src/BestSeller.js
+++ src/BestSeller.js
@@ -1,61 +1,55 @@
-import React from 'react';
+import React from "react";
 import Product from "./Product";
 import "./bestseller.css";
 import tengol from "./tengol.png";
 
 function BestSeller() {
-    return (
-        <div className="best_seller">
-            <h3 className="best_seller_title">BEST SELLERS</h3>
-            <div className="best_seller_list">
-
+  return (
+    <div className="best_seller">
+      <h3 className="best_seller_title">BEST SELLERS</h3>
+      <div className="best_seller_list">
         <div className="best_seller_products">
-        <Product
-
+          <Product
             id="1"
             title="ಬೇಸನ್ ಉಂಡೆ | Besan Laddoo"
-            category= "box"
+            category="box"
             image="https://cdn.shopify.com/s/files/1/0398/6545/0645/products/69724367_2483046198423304_3890100649064398848_o-removebg-preview_1024x1024.png?v=1593273118"
-            price="72"
-            
+            price="90"
           />
-          </div>
+        </div>
 
-          <div className="best_seller_products">
+        <div className="best_seller_products">
           <Product
             id="2"
             title="ಪುರಿ ಉಂಡೆ | Puri Unde"
-            category= "box"
+            category="box"
             image="https://m.media-amazon.com/images/I/41hzY651KAL._AC_UL480_QL65_.jpg"
-            price="78"
-            
+            price="96"
           />
-          </div>
-          
+        </div>
 
-          <div className="best_seller_products">
+        <div className="best_seller_products">
           <Product
             id="10"
             title="ತೆಂಗೊಳಲು | Tengolu"
-            category= "box"
+            category="box"
             image={tengol}
-            price="60"
+            price="72"
           />
-          </div>
-            
-          <div className="best_seller_products">
+        </div>
+
+        <div className="best_seller_products">
           <Product
             id="8"
             title="ನಿಪ್ಪಟ್ಟು | Nippattu"
-            category= "box"
+            category="box"
             image="https://m.media-amazon.com/images/I/61IJJ+JrXXL._AC_UL480_QL65_.jpg"
-            price="36"
+            price="48"
           />
-          </div>
-
-          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default BestSeller;
